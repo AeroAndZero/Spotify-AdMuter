2dfed857a9efc90b3edeccad666b3077d3a284a2
AeroAndZero <ayushthakur667@gmail.com>
2020-04-17 01:28:17 +0530

    Just that 0.1% error

--- content.js
+++ content.js
@@ -13,26 +13,34 @@ chrome.runtime.onMessage.addListener(function(msg){
 
 window.onload = (event) => {
   pageLoad = true;
-  volumeButton = document.querySelector('[aria-label="Mute"]');
+  try{
+  	volumeButton = document.querySelector('[aria-label="Mute"]');
+  }catch{
+  	alert("Extension or Page didn't load correctly, please refresh the page");
+  }
 };
 
 window.setInterval( function(){
-	if(isRunning && pageLoad){
-		let songDurationClasses = document.getElementsByClassName('playback-bar__progress-time')
-		songDuration = songDurationClasses[1];
-		songMinutes = parseInt(songDuration.innerText[0]);
-		songSeconds = parseInt(songDuration.innerText.split(":")[1]);
-		if(songMinutes < 1 && songSeconds <= 30){
-			if(!isMuted){
-				volumeButton.click()
-				isMuted = true
-				console.log("Ad Muted")
-			}
-		}else{
-			if(isMuted){
-				volumeButton.click();
-				isMuted = false;
+	try{
+		if(isRunning && pageLoad){
+			let songDurationClasses = document.getElementsByClassName('playback-bar__progress-time')
+			songDuration = songDurationClasses[1];
+			songMinutes = parseInt(songDuration.innerText[0]);
+			songSeconds = parseInt(songDuration.innerText.split(":")[1]);
+			if(songMinutes < 1 && songSeconds <= 30){
+				if(!isMuted){
+					volumeButton.click()
+					isMuted = true
+					console.log("Ad Muted")
+				}
+			}else{
+				if(isMuted){
+					volumeButton.click();
+					isMuted = false;
+				}
 			}
 		}
+	}catch{
+			alert("Extension or Page didn't load correctly, please refresh the page");
 	}
 },1000)
